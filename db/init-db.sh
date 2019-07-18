56cf8629477e6474039472a5d3a02b54b7ddf092
John Dalziel <john@crashposition.com>
2019-07-18 19:44:15 +0100

    Datasbe import complete

--- db/init-db.sh
+++ db/init-db.sh
@@ -14,7 +14,7 @@ CREATE_RAW_TABLE_SQL=$(cat <<EOF
     CREATE TABLE raw (
         product_name TEXT NOT NULL,
         product_sku UUID NOT NULL,
-        advertiser_name TEXT NOT NULL);
+        advertiser_name TEXT NOT NULL );
 EOF
 )
 
@@ -51,7 +51,7 @@ CREATE_PRODUCT_SKUS_TABLE_SQL=$(cat <<EOF
 EOF
 )
 
-# Populate final tables
+# Populate tables
 POPULATE_ADVERTISERS_TABLE_SQL=$(cat <<EOF
     INSERT INTO advertisers ( advertiser_name)
         SELECT distinct(advertiser_name) FROM raw;
@@ -64,9 +64,40 @@ POPULATE_PRODUCTS_TABLE_SQL=$(cat <<EOF
 EOF
 )
 
-POPULATE_PRODUCT_SKUS_TABLE_SQL=$(cat <<EOF
+# Update raw data
+ADD_PRODUCT_ID_COLUMN_TO_RAW_SQL=$(cat <<EOF
+    ALTER TABLE raw ADD COLUMN product_id INTEGER;
+EOF
+)
+ADD_PRODUCT_IDS_COLUMN_TO_RAW_SQL=$(cat <<EOF
+    UPDATE raw SET product_id=products.product_id FROM products
+        WHERE raw.product_name=products.product_name;
+EOF
+)
+ADD_ADVERTISER_ID_COLUMN_TO_RAW_SQL=$(cat <<EOF
+    ALTER TABLE raw ADD COLUMN advertiser_id INTEGER;
+EOF
+)
+ADD_ADVERTISER_IDS_COLUMN_TO_RAW_SQL=$(cat <<EOF
+    UPDATE raw SET advertiser_id=advertisers.advertiser_id FROM advertisers
+        WHERE raw.advertiser_name=advertisers.advertiser_name;
+EOF
+)
+
+# Product SKUs
+POPULATE_PRODUCT_SKUS_TABLE_1_SQL=$(cat <<EOF
     INSERT INTO product_skus ( product_sku )
         SELECT distinct(product_sku) FROM raw;
+EOF
+)
+POPULATE_PRODUCT_SKUS_TABLE_2_SQL=$(cat <<EOF
+    UPDATE product_skus SET product_id=raw.product_id FROM raw
+        WHERE raw.product_sku=product_skus.product_sku;
+EOF
+)
+POPULATE_PRODUCT_SKUS_TABLE_3_SQL=$(cat <<EOF
+    UPDATE product_skus SET advertiser_id=raw.advertiser_id FROM raw
+        WHERE raw.product_sku=product_skus.product_sku;
 EOF
 )
 
@@ -92,6 +123,8 @@ echo "Please wait... populating advertisers"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 $CREATE_ADVERTISERS_TABLE_SQL
 $POPULATE_ADVERTISERS_TABLE_SQL
+$ADD_ADVERTISER_ID_COLUMN_TO_RAW_SQL
+$ADD_ADVERTISER_IDS_COLUMN_TO_RAW_SQL
 commit;
 EOSQL
 
@@ -101,6 +134,8 @@ echo "Please wait... populating products"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 $CREATE_PRODUCTS_TABLE_SQL
 $POPULATE_PRODUCTS_TABLE_SQL
+$ADD_PRODUCT_ID_COLUMN_TO_RAW_SQL
+$ADD_PRODUCT_IDS_COLUMN_TO_RAW_SQL
 commit;
 EOSQL
 
@@ -109,18 +144,20 @@ echo "Please wait... populating product skus"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 $CREATE_PRODUCT_SKUS_TABLE_SQL
-$POPULATE_PRODUCT_SKUS_TABLE_SQL
+$POPULATE_PRODUCT_SKUS_TABLE_1_SQL
+$POPULATE_PRODUCT_SKUS_TABLE_2_SQL
+$POPULATE_PRODUCT_SKUS_TABLE_3_SQL
 commit;
 EOSQL
 
 
-
+echo "Please wait... cleaning up"
 
 # Remove the raw table
-#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-#$DROP_RAW_TABLE_SQL
-#commit;
-#EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+$DROP_RAW_TABLE_SQL
+commit;
+EOSQL
 
 echo "Database ready"
 exit 0
